496948c25a554fbfc2233e6a9ab28fb0372e9046
FrederikVK <frederik_kristensen@hotmail.com>
2025-11-01 20:54:33 +0100

    bugfix: correct path to environment.yml

--- setup_env.sh
+++ setup_env.sh
@@ -4,6 +4,6 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate base
 yes | conda env remove --name pace-predictor-env
 
-conda env create --file envs/environment.yml
+conda env create --file environment.yml
 conda activate pace-predictor-env
 python -m ipykernel install --user --name your-env-name --display-name "Python (pace-predictor-env)"
