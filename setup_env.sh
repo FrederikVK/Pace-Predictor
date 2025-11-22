b41588c54aac04eba4f91a94ad98dfbe88461ec6
FrederikVK <34678108+FrederikVK@users.noreply.github.com>
2025-11-22 21:15:32 +0100

    Codespaces/setup (#1)
    * docker and devcontainer
    
    * readme

--- setup_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate base
-yes | conda env remove --name pace-predictor-env
-
-conda env create --file environment.yml
-conda activate pace-predictor-env
-python -m ipykernel install --user --name your-env-name --display-name "Python (pace-predictor-env)"
